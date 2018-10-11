02e2cbb28c0c7329150a05e916fe461ccb48c39d
nilmohan <nilakantha18@gmail.com>
2018-10-11 23:03:29 +0530

    updated server js

--- server/server.js
+++ server/server.js
@@ -1,30 +1,14 @@
 const path = require('path');
 const express = require('express');
-const corsPrefetch = require('cors-prefetch-middleware');
-const imagesUpload = require('images-upload-middleware');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.use(corsPrefetch);
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.post('/multiple', imagesUpload(
-    './server/static/multipleFiles',
-    'http://localhost:9090/static/multipleFiles',
-    true
-));
-
-app.post('/notmultiple', imagesUpload(
-    './server/static/files',
-    'http://localhost:9090/static/files'
-));
-
 app.listen(port, () => {
   console.log('Server is up!');
 });
